8cdb1402f57fbca781e2421e09b7566c8f21c3bb
SashaCerbai <cerbai03@gmail.com>
2020-12-15 10:52:08 +0100

    yes

---
+++ src/estpsit/EsTpsit.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package estpsit;
+
+/**
+ *
+ * @author cerba
+ */
+public class EsTpsit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+   
+        int contatore= 0;
+    
+         for(int i=1; i<11; i++){
+    
+            contatore+=1;    
+    
+            System.out.println(contatore);
+    
+        }
+    
+    }
+    
+    
+    
+}
